4604c715e92a7ac9b12f8da788c4b6a07f3cd00c
lisadelbes <lisadelbes@hotmail.fr>
2025-09-25 17:01:20 +0200

    ajout database

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,13 +2,6 @@ export const environment = {
 
 firebaseConfig: {
 
-apiKey: "AIzaSyC8DNWUjedltLkBoOc7FhSG1H4TzbYm9Kk",
-authDomain: "lisa-s-project-37839.firebaseapp.com",
-databaseURL: "https://lisa-s-project-37839-default-rtdb.europe-west1.firebasedatabase.app",
-projectId: "lisa-s-project-37839",
-storageBucket: "lisa-s-project-37839.firebasestorage.app",
-messagingSenderId: "898218038354",
-appId: "1:898218038354:web:a8b23eeecc3b23a9797b8b"
     
     }
 }
